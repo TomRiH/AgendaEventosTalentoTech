b5924640cece8cd0af7403988b46ae7d2266511c
tomas <tomas.riosh@hotmail.com>
2024-11-21 19:47:32 -0500

    Primer commit

---
+++ src/main/java/com/TalentoTech/AgendadorEventos/AgendadorEventosApplication.java
@@ -0,0 +1,13 @@
+package com.TalentoTech.AgendadorEventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TalentoTech/AgendadorEventos/AgendadorEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TalentoTech.AgendadorEventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
