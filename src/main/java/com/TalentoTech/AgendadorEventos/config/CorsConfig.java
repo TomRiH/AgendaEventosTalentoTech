e70c36859f6d681d51384ee4a2a65452a6df2cf8
Jefferson <jeffer230@gmail.com>
2024-11-30 03:19:40 -0500

    config cors

---
+++ src/main/java/com/TalentoTech/AgendadorEventos/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.TalentoTech.AgendadorEventos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permitir todas las rutas
+                .allowedOrigins("http://localhost:4200") // Permitir este origen específico
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permitir todos los encabezados
+                .allowCredentials(true); // Permitir cookies o credenciales
+    }
+}
